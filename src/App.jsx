9037b89acbf0fa0cd14f2899da484fad85257666
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2024-04-02 12:40:32 +0530

    reviewer list and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Landing from "./Pages/Home/Landing";
 
 import { Routes, Route } from "react-router-dom";
 import Satyam from "./Pages/Satyam/Satyam";
+import Reviewer from "./Pages/Reviewer/Reviewer";
+import Dashboard from "./Pages/Reviewer/Dashboard";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<Landing />} />
         <Route path="/satyam/*" element={<Satyam />} />
+        <Route path="/reviewer" element={<Reviewer />} />
+        <Route path="/reviewer/dashboard/:id" element={<Dashboard />} />
       </Routes>
     </>
   );
